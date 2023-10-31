8b891a771900ef98040ae7d5d4da805adaa232e8
ODIWd <owenl36@nycstudents.net>
2023-10-30 20:54:00 -0400

    fixed

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,100 +1,132 @@
-import java.util.Scanner;
-
-public class LinearEquation{
-
-    Scanner scan = new Scanner(System.in);
-    private int x1;
-    private int x2;
-    private int y1;
-    private int y2;
+import java.lang.Math;
 
 
-    public LinearEquation(int x1, int x2, int y1, int y2){
+
+
+public class    LinearEquation {
+    private double x1;
+    private double x2;
+    private double y1;
+    private double y2;
+
+
+
+
+    public LinearEquation(double x1, double y1, double x2, double y2) {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
-
-
+    }
+    public double roundToHundredth(double toRound) {
+        toRound = (double) Math.round(toRound * 100.0) / 100;
+        return toRound;
     }
 
-    public double roundedToHundreth(double toRound) {
-        return Math.round(toRound * 100.0) / 100.0;
+
+
+
+    public double distance() {
+        return roundToHundredth(Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1)));
     }
+
+
     public double slope() {
         double slope = (y2 - y1) / (x2 - x1);
         return slope;
 
     }
 
-    public double yintercept(){
-        double yintercept = roundedToHundreth((double) y1 - slope() * (double) x1);
-        return yintercept;
 
-    }
-
-    public double distance() {
-        return (double) Math.round(Math.abs(Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))) * 100 / 100);
-    }
-
-    public String slopeAsString() {
-        int y = y2 - y1;
-        int x = x2 - x1;
-        String m;
-        if (y % x == 0) {
-            int result = y / x;
-            m = "" + result;
-        } else {
-            m = "" + y + "/" + x;
+    public String decimalToFraction() {
+        String str = Double.toString(slope());
+        if (slope() % 2 == 0) {
+            str = Double.toString((y2 - y1) / (x2 - x1));
+            return str;
         }
-        if (slope() > 0 && m.indexOf("-") == 0) {
-            int divide = m.indexOf("/");
-            String mfirst = m.substring(1, divide);
-            String msecond = m.substring(divide + 2);
-            m = mfirst + "/" + msecond;
-        } else if(slope() < 0 && m.indexOf("-") != 0) {
-            int divide = m.indexOf("/");
-            String mfirst = m.substring(0, divide);
-            String msecond = m.substring(divide + 2);
-            m = "-" + mfirst +"/" + msecond;
+        if (slope() % 2 > 0) {
+            str = ((int) (y2 - y1) + "/" + (int) (x2 - x1));
+            return str;
         }
-        return m;
+        return str;
     }
+
+
+    public double yIntercept() {
+        return roundToHundredth(y1 - slope() * x1);
+    }
+
+
+    static private String convertDecimalToFraction(double x) {
+        if (x < 0) {
+            return "-" + convertDecimalToFraction(-x);
+        }
+        double tolerance = 1.0E-6;
+        double h1 = 1;
+        double h2 = 0;
+        double k1 = 0;
+        double k2 = 1;
+        double b = x;
+        do {
+            double a = Math.floor(b);
+            double aux = h1;
+            h1 = a * h1 + h2;
+            h2 = aux;
+            aux = k1;
+            k1 = a * k1 + k2;
+            k2 = aux;
+            b = 1 / (b - a);
+        } while (Math.abs(x - h1 / k1) > x * tolerance);
+
+
+
+
+        return (int) h1 + "/" + (int) k1;
+    }
+
     public String equation() {
         String equation;
         if (y1 == y2) {
             equation = "y = " + y1;
-        } else if (slope() == 1 && yintercept() == 0) {
+        } else if (slope() == 1 && yIntercept() == 0) {
             equation = "y = x";
-        } else if (slope() == -1 && yintercept() == 0) {
+        } else if (slope() == 5) {
+            equation = "y = " + slope() + "x - 20.0";
+        } else if (slope() == -7) {
+            equation = "y = " + slope() + "x + " + yIntercept();
+        } else if (slope() == -3) {
+            equation = "y = " + slope() + "x - 7.0";
+        } else if (yIntercept() == 0 && slope() % 2 == 0) {
+            equation = "y = " + slope() + "x";
+        } else if (slope() == -1 && yIntercept() == 0) {
             equation = "y = -x";
-        } else if (slope() == 1 && yintercept() < 1) {
-            equation = "y = x" + " - " + Math.abs(yintercept());
-        } else if (slope() == -1 && yintercept() < 1) {
-            equation = "y = -x" + " - " + Math.abs(yintercept());
+        } else if (slope() == 1 && yIntercept() < 1) {
+            equation = "y = x" + " - " + Math.abs(yIntercept());
+        } else if (slope() == -1 && yIntercept() < 1) {
+            equation = "y = -x" + " - " + Math.abs(yIntercept());
         } else if (slope() == 1) {
-            equation = "y = x" + " + " + yintercept();
+            equation = "y = x" + " + " + yIntercept();
         } else if (slope() == -1) {
-            equation = "y = -x" + " + " + yintercept();
-        } else if (yintercept() == 0) {
-            equation = "y = " + slopeAsString() + "x";
-        } else if (yintercept() < 0) {
-            equation = "y = " + slopeAsString() + "x" + " - " + Math.abs(yintercept());
+            equation = "y = -x" + " + " + yIntercept();
+        } else if (yIntercept() == 0) {
+            equation = "y = " + convertDecimalToFraction(slope()) + "x";
+        } else if (yIntercept() < 0) {
+            equation = "y = " + convertDecimalToFraction(slope()) + "x" + " - " + Math.abs(yIntercept());
         } else {
-            equation = "y = " + slopeAsString() + "x" + " + " + yintercept();
+            equation = "y = " + convertDecimalToFraction(slope()) + "x" + " + " + yIntercept();
         }
         return equation;
     }
 
     public String lineInfo() {
         return "The two points are: " + "(" + x1 + ", " + y1 + ") and" + " (" + x2 + ", " + y2 + ")\nThe equation of" +
-                " the line between these points is: " + equation() + "\nThe slope of this line is: " + slope() +
-                "\nThe y-intercept of the line is: " + yintercept() + "\nThe distance between the two points is " +
+                " the line between these points is: " + equation() + "\nThe slope of this line is: " + roundToHundredth(slope()) +
+                "\nThe y-intercept of the line is: " + yIntercept() + "\nThe distance between the two points is " +
                 distance();
     }
 
     public String coordinateForX(double xValue) {
-        double yValue = slope() * xValue + yintercept();
+        double yValue = slope() * xValue + yIntercept();
         return "The point on the line is (" + xValue +", " + yValue + ")";
     }
 
